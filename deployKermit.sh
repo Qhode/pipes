5aa29980590b12b40426354ebb6557729458e9f7
Avi Cavale <avic@jfrog.com>
2019-04-19 10:05:51 -0700

    adding kermit_deploy

---
+++ deployKermit.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -e
+
+set_context() {
+  echo "CURR_JOB=$JOB_NAME"
+  echo "DEPLOY_VERSION=$DEPLOY_VERSION"
+
+  echo "BASTION_USER=$BASTION_USER"
+  echo "BASTION_IP=$BASTION_IP"
+  echo "ONEBOX_USER=$ONEBOX_USER"
+  echo "ONEBOX_IP=$ONEBOX_IP"
+}
+
+configure_node_creds() {
+  echo "Extracting AWS PEM"
+  echo "-----------------------------------"
+  pushd $(shipctl get_resource_meta "$RES_PEM")
+  if [ ! -f "integration.json" ]; then
+    echo "No credentials file found at location: $RES_PEM"
+    return 1
+  fi
+
+  cat integration.json | jq -r '.key' > key.pem
+  chmod 600 key.pem
+
+  echo "Completed Extracting AWS PEM"
+  echo "-----------------------------------"
+
+  ssh-add key.pem
+  echo "SSH key added successfully"
+  echo "--------------------------------------"
+  popd
+}
+
+pull_ribbit_repo() {
+  echo "Pull admiral-repo started"
+  local PULL_CMD="git -C /home/ubuntu/ribbit pull origin master"
+  ssh -A $BASTION_USER@$BASTION_IP ssh $SWARM_USER@$SWARM_IP "$PULL_CMD"
+  echo "Successfully pulled admiral-repo"
+}
+
+deploy() {
+  echo "Deploying the release $DEPLOY_VERSION to OneBox"
+  echo "--------------------------------------"
+
+  echo "SSH key file list"
+  ssh-add -L
+
+  local inspect_command="ip addr"
+  echo "Executing inspect command: $inspect_command"
+  ssh -A $BASTION_USER@$BASTION_IP ssh $ONEBOX_USER@$ONEBOX_IP "$inspect_command"
+  echo "-------------------------------------="
+
+#  local deploy_command="sudo /home/ubuntu/ribbit/ribbit.sh upgrade"
+#  echo "Executing deploy command: $deploy_command"
+#  ssh -A $BASTION_USER@$BASTION_IP ssh $ONEBOX_USER@$ONEBOX_IP "$deploy_command"
+#  echo "-------------------------------------="
+
+  echo "Successfully deployed release $DEPLOY_VERSION to Onebox env"
+}
+
+create_version() {
+  echo "Creating a state file for" $CURR_JOB
+  # create a state file so that next job can pick it up
+  echo "versionName=$DEPLOY_VERSION" > "$JOB_STATE/$CURR_JOB.env" #adding version state
+  echo "Completed creating a state file for" $CURR_JOB
+}
+
+main() {
+  eval $(ssh-agent -s)
+  set_context
+  configure_node_creds
+#  pull_ribbit_repo
+  deploy
+#  create_version
+}
+
+main
